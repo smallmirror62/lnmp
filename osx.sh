48690c73babe0e012c532ca1d29a63d401817e9a
Xu Tongle <xutongle@gmail.com>
2015-08-04 23:35:05 +0800

    更新苹果安装版本

--- osx.sh
+++ osx.sh
@@ -1,12 +1,8 @@
-##安装依赖
-````shell
-sudo -s
-LANG=C
-yum -y install patch make cmake gcc gcc-c++ gcc-g77 bison flex file libtool libtool-libs autoconf kernel-devel libjpeg libjpeg-devel libpng libpng-devel gd gd-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel gmp-devel gettext gettext-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers vim-minimal nano fonts-chinese gmp-devel pspell-devel unzip libcap diffutils readline-devel libxslt libxslt-devel
-````
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
-##编译安装PHP所需的支持库
-````shell
+cd ~/Download
 mkdir -p software
 cd software
 wget -c http://7xk96f.com1.z0.glb.clouddn.com/software/luajit/LuaJIT-2.0.4.tar.gz
@@ -63,68 +59,23 @@ cd pcre-8.37
 make && make install
 cd ../
 
-````
-
-##编译安装PHP
-````shell
 tar zxf php-5.6.11.tar.gz
 cd php-5.6.11/
 ./configure --prefix=/usr --mandir=/usr/share/man --infodir=/usr/share/info --sysconfdir=/private/etc --with-apxs2=/usr/sbin/apxs --with-config-file-path=/etc --with-config-file-scan-dir=/private/etc/php --with-libxml-dir --with-openssl --with-kerberos --with-zlib --enable-bcmath --with-bz2 --enable-calendar --with-curl --enable-exif --enable-fpm --enable-ftp --with-png-dir --with-gd --with-jpeg-dir --enable-gd-native-ttf --with-icu-dir --enable-mbstring --enable-mbregex --enable-shmop --enable-soap --enable-sockets --enable-sysvmsg --enable-sysvsem --enable-sysvshm --enable-wddx --with-xmlrpc --with-readline --with-iconv-dir --with-xsl --enable-zip --with-pcre-regex --with-freetype-dir --enable-xml --with-mysql=mysqlnd --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-pdo-sqlite --with-sqlite3 --disable-rpath  --enable-inline-optimization --with-mcrypt  --with-mhash --enable-pcntl --enable-sockets  --without-pear --with-gettext --enable-fileinfo --enable-opcache --enable-cli
 make ZEND_EXTRA_LIBS='-liconv'
 make install
 
-cp php.ini-production /etc/php.ini
-cp /etc/php-fpm.conf.default /etc/php-fpm.conf
+cp php.ini-production /private/etc/php.ini
+cp /private/etc/php-fpm.conf.default /private/etc/php-fpm.conf
 
-````
-##编译安装Nginx
 
-````shell
-#编译tengine（推荐）
 tar zxf tengine-2.1.0.tar.gz
 cd tengine-2.1.0
 ./configure --prefix=/usr --sbin-path=/usr/sbin --dso-path=/usr/lib/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/private/var/log/nginx/access.log --error-log-path=/private/var/log/nginx/error.log --lock-path=/private/var/lock/nginx.lock --pid-path=/private/var/run/nginx.pid --http-client-body-temp-path=/private/var/lib/nginx/body --http-fastcgi-temp-path=/private/var/lib/nginx/fastcgi --http-proxy-temp-path=/private/var/lib/nginx/proxy --http-scgi-temp-path=/private/var/lib/nginx/scgi --http-uwsgi-temp-path=/private/var/lib/nginx/uwsgi --with-luajit-inc=/usr/local/include/luajit-2.0 --with-luajit-lib=/usr/local/lib --with-ipv6 --with-mail --with-pcre-jit --with-http_ssl_module --with-http_sub_module --with-http_flv_module --with-mail_ssl_module --with-http_dav_module --with-http_xslt_module --with-http_spdy_module --with-http_geoip_module --with-http_realip_module --with-http_addition_module --with-http_gzip_static_module --with-http_stub_status_module --with-http_image_filter_module --with-http_lua_module=shared --with-http_footer_filter_module=shared --with-http_sysguard_module=shared --with-http_limit_req_module=shared --with-http_trim_filter_module=shared --with-http_upstream_ip_hash_module=shared --with-http_upstream_least_conn_module=shared --with-http_upstream_session_sticky_module=shared --with-http_concat_module=shared --with-cc-opt="-Wno-deprecated-declarations"
 
-#无GEOIP http_image_filter_module
-./configure --prefix=/usr --sbin-path=/usr/sbin --dso-path=/usr/lib/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/private/var/log/nginx/access.log --error-log-path=/private/var/log/nginx/error.log --lock-path=/private/var/lock/nginx.lock --pid-path=/private/var/run/nginx.pid --http-client-body-temp-path=/private/var/lib/nginx/body --http-fastcgi-temp-path=/private/var/lib/nginx/fastcgi --http-proxy-temp-path=/private/var/lib/nginx/proxy --http-scgi-temp-path=/private/var/lib/nginx/scgi --http-uwsgi-temp-path=/private/var/lib/nginx/uwsgi --with-luajit-inc=/usr/local/include/luajit-2.0 --with-luajit-lib=/usr/local/lib --with-ipv6 --with-mail --with-pcre-jit --with-http_ssl_module --with-http_sub_module --with-http_flv_module --with-mail_ssl_module --with-http_dav_module --with-http_xslt_module --with-http_spdy_module --with-http_realip_module --with-http_addition_module --with-http_gzip_static_module --with-http_stub_status_module --with-http_lua_module=shared --with-http_footer_filter_module=shared --with-http_sysguard_module=shared --with-http_limit_req_module=shared --with-http_trim_filter_module=shared --with-http_upstream_ip_hash_module=shared --with-http_upstream_least_conn_module=shared --with-http_upstream_session_sticky_module=shared --with-http_concat_module=shared --with-cc-opt="-Wno-deprecated-declarations"
-
-#增加第三方模块
---add-module=/root/software/ngx_http_substitutions_filter_module
-
 make && make install
 cd ../
 
-#编译原版NGINX
-tar zxvf nginx-0.8.46.tar.gz
-cd nginx-0.8.46/
-./configure --user=www --group=www --prefix=/usr/local/nginx --with-http_stub_status_module --with-http_ssl_module
-make && make install
-cd ../
-
-ln -s /etc/nginx/sites-available/localhost.conf /etc/nginx/sites-enabled/localhost.conf 
-ln -s /etc/nginx/sites-available/pma.l68.net.conf /etc/nginx/sites-enabled/pma.l68.net.conf
-ln -s /etc/nginx/sites-available/leaps.l68.net.conf /etc/nginx/sites-enabled/leaps.l68.net.conf
-ln -s /etc/nginx/sites-available/yuncms.l68.net.conf /etc/nginx/sites-enabled/yuncms.l68.net.conf
-ln -s /etc/nginx/sites-available/tintsoft.l68.net.conf /etc/nginx/sites-enabled/tintsoft.l68.net.conf
-
-````
-##收尾工作
-````shell
-
-/usr/sbin/groupadd www
-/usr/sbin/useradd -m -g www www
-
-#设置自动启动
-chkconfig nginx on
-chkconfig php-fpm on
-
-#创建网站和日志目录并赋予权限
-mkdir /home/www/htdocs
-mkdir /home/www/logs
-chown -R www:www /home/www/htdocs
-chmod -R 755 /home/www/htdocs
-chown -R www:www /home/www/logs
-chmod -R 755 /home/www/logs
 
 chown root /Library/LaunchDaemons/org.php-fpm.plist
 
