007b50cb7ddd11f1fdf457c3459c0e1c19b9d89c
XuTongle <xutongle@gmail.com>
2015-08-06 15:35:09 +0800

    a
    Signed-off-by: XuTongle <xutongle@gmail.com>

--- centos.sh
+++ centos.sh
@@ -8,7 +8,136 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
-yum -y install patch make cmake gcc gcc-c++ gcc-g77 bison flex file libtool libtool-libs autoconf kernel-devel libjpeg libjpeg-devel libpng libpng-devel gd gd-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel gmp-devel gettext gettext-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers vim-minimal nano fonts-chinese gmp-devel pspell-devel unzip libcap diffutils readline-devel libxslt libxslt-devel
+clear
+echo "========================================================================="
+echo "LNMP V1.0 for CentOS/RadHat Linux Server, TintSoft by Licess"
+echo "========================================================================="
+echo "A tool to auto-compile & install Nginx+MySQL+PHP on Linux "
+echo ""
+echo "For more information please visit http://www.tintsoft.com/"
+echo "========================================================================="
+cur_dir=$(pwd)
+
+function InitInstall()
+{
+	cat /etc/issue
+	uname -a
+	MemTotal=`free -m | grep Mem | awk '{print  $2}'`  
+	echo -e "\n Memory is: ${MemTotal} MB "
+	#Set timezone
+	rm -rf /etc/localtime
+	ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+	yum install -y ntp
+	ntpdate -u pool.ntp.org
+	date
+
+	rpm -qa|grep httpd
+	rpm -e httpd
+	rpm -qa|grep mysql
+	rpm -e mysql
+	rpm -qa|grep php
+	rpm -e php
+
+	yum -y remove httpd*
+	yum -y remove php*
+	yum -y remove mysql-server mysql mysql-libs
+	yum -y remove php-mysql
+
+	yum -y install yum-fastestmirror
+	yum -y remove httpd
+	#yum -y update
+
+	#Disable SeLinux
+	if [ -s /etc/selinux/config ]; then
+	sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+	fi
+
+	cp /etc/yum.conf /etc/yum.conf.lnmp
+	sed -i 's:exclude=.*:exclude=:g' /etc/yum.conf
+
+	for packages in patch make cmake gcc gcc-c++ gcc-g77 bison flex file libtool libtool-libs autoconf kernel-devel libjpeg libjpeg-devel libpng libpng-devel gd gd-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel gmp-devel gettext gettext-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers vim-minimal nano fonts-chinese gmp-devel pspell-devel unzip libcap diffutils readline-devel libxslt libxslt-devel
+	do yum -y install $packages; done
+}
+function CheckAndDownloadFiles()
+{
+	echo "============================check files=================================="
+if [ -s libiconv-1.14.tar.gz ]; then
+  echo "libiconv-1.14.tar.gz [found]"
+  else
+  echo "Error: libiconv-1.14.tar.gz not found!!!download now......"
+  wget -c http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz
+fi
+
+if [ -s libmcrypt-2.5.8.tar.gz ]; then
+  echo "libmcrypt-2.5.8.tar.gz [found]"
+  else
+  echo "Error: libmcrypt-2.5.8.tar.gz not found!!!download now......"
+  wget -c http://7xk96f.com1.z0.glb.clouddn.com/software/mcrypt/libmcrypt-2.5.8.tar.gz
+fi
+
+if [ -s mhash-0.9.9.9.tar.gz ]; then
+  echo "mhash-0.9.9.9.tar.gz [found]"
+  else
+  echo "Error: mhash-0.9.9.9.tar.gz not found!!!download now......"
+  wget -c http://7xk96f.com1.z0.glb.clouddn.com/software/mhash/mhash-0.9.9.9.tar.gz
+fi
+
+if [ -s mcrypt-2.6.8.tar.gz ]; then
+  echo "mcrypt-2.6.8.tar.gz [found]"
+  else
+  echo "Error: mcrypt-2.6.8.tar.gz not found!!!download now......"
+  wget -c http://7xk96f.com1.z0.glb.clouddn.com/software/mcrypt/mcrypt-2.6.8.tar.gz
+fi
+
+}
+
+function InstallDependsAndOpt()
+{
+cd $cur_dir
+
+tar zxvf libiconv-1.14.tar.gz
+cd libiconv-1.14/
+./configure
+make && make install
+cd ../
+
+cd $cur_dir
+tar zxf libmcrypt-2.5.8.tar.gz
+cd libmcrypt-2.5.8/
+./configure
+make && make install
+/sbin/ldconfig
+cd libltdl/
+./configure --enable-ltdl-install
+make && make install
+cd ../../
+
+cd $cur_dir
+tar zxf mhash-0.9.9.9.tar.gz
+cd mhash-0.9.9.9/
+./configure
+make && make install
+cd ../
+
+ln -s /usr/local/lib/libmcrypt.la /usr/lib/libmcrypt.la
+ln -s /usr/local/lib/libmcrypt.so /usr/lib/libmcrypt.so
+ln -s /usr/local/lib/libmcrypt.so.4 /usr/lib/libmcrypt.so.4
+ln -s /usr/local/lib/libmcrypt.so.4.4.8 /usr/lib/libmcrypt.so.4.4.8
+ln -s /usr/local/lib/libmhash.a /usr/lib/libmhash.a
+ln -s /usr/local/lib/libmhash.la /usr/lib/libmhash.la
+ln -s /usr/local/lib/libmhash.so /usr/lib/libmhash.so
+ln -s /usr/local/lib/libmhash.so.2 /usr/lib/libmhash.so.2
+ln -s /usr/local/lib/libmhash.so.2.0.1 /usr/lib/libmhash.so.2.0.1
+
+cd $cur_dir
+tar zxf mcrypt-2.6.8.tar.gz
+cd mcrypt-2.6.8/
+./configure
+make && make install
+cd ../
+
+}
 
 mkdir -p software
 cd software
@@ -24,11 +153,8 @@ wget -c http://7xk96f.com1.z0.glb.clouddn.com/software/nginx/nginx-1.9.3.tar.gz
 wget -c http://7xk96f.com1.z0.glb.clouddn.com/software/pcre/pcre2-10.20.tar.gz
 wget -c http://7xk96f.com1.z0.glb.clouddn.com/software/pcre/pcre-8.37.tar.gz
 
-tar zxvf libiconv-1.14.tar.gz
-cd libiconv-1.14/
-./configure
-make && make install
-cd ../
+#
+
 
 tar zxvf libmcrypt-2.5.8.tar.gz 
 cd libmcrypt-2.5.8/
@@ -48,9 +174,6 @@ cd ../
 
 tar zxvf mcrypt-2.6.8.tar.gz
 cd mcrypt-2.6.8/
-export LD_LIBRARY_PATH=/usr/local/lib
-export LDFLAGS="-L/usr/local/lib/ -I/usr/local/include/"
-export CFLAGS="-I/usr/local/include/"
 ./configure
 make && make install
 cd ../
